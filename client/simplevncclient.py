e9b33b9cb253ef622b14d34941175f61c1c0142e
MunteanuAlexandru02 <55241301+MunteanuAlexandru02@users.noreply.github.com>
2023-01-18 10:00:43 +0200

    Add files via upload

--- client/simplevncclient.py
+++ client/simplevncclient.py
@@ -9,6 +9,17 @@ from enum import Enum
 import asyncio ###
 import asyncvnc.asyncvnc as asyncvnc
 from PIL import Image
+
+#   Basically, the first page of the
+#   client will do almost the same thing as the server
+#   and that is to provide a simple GUI logging screen for the user.
+#   The only main difference is the "Server Address" text box.
+#   For a simple way to test the client and the server we recommend to
+#   start the server on your own machine and introduce in the
+#   "Server Address" zone: localhost, that should start the "SimpleVNC"
+#   on your machine.
+
+#client status
 class Status(Enum):
 
     RUNNING = 1
@@ -41,34 +52,35 @@ parameter_list_column = [
     ],
 
 	[
-        sg.Text("Server Adress", expand_x=True),
+        sg.Text("Server Adress", expand_x = True),
 
-        sg.In(size=(25, 1), enable_events=True, key="-ADDR-"),
+        sg.In(size = (25, 1), enable_events = True, key = "-ADDR-")
 
     ],
     [
-        sg.Text("Port", expand_x=True),
+        sg.Text("Port", expand_x = True),
 
-        sg.In(size=(25, 1), enable_events=True, key="-PORT-"),
-
-    ],
-
-    [
-        sg.Text("Password", expand_x=True),
-
-        sg.In(size=(25, 1), enable_events=True, key="-PASSWORD-", password_char='*'),
+        sg.In(size = (25, 1), enable_events = True, key = "-PORT-")
 
     ],
 
     [
-        sg.Button(button_text="Connect", button_color="#275DA4", size=10, key="-CONNECT-"),
+        sg.Text("Password", expand_x = True),
 
-        sg.Button(button_text="Disconnect", button_color="red", size=10, key="-DISCONNECT-")
+        sg.In(size = (25, 1), enable_events = True, key = "-PASSWORD-", password_char = '*')
+
     ],
 
     [
-        sg.Text(" ", expand_x=True),
-        sg.Button(button_text="Quit", button_color="#808080", key="-QUIT-")
+        sg.Button(button_text = "Connect", button_color = "#275DA4", size = 10, key = "-CONNECT-"),
+
+        sg.Button(button_text = "Disconnect", button_color = "red", size = 10, key = "-DISCONNECT-")
+    ],
+
+    [
+        sg.Text(" ", expand_x = True),
+
+        sg.Button(button_text = "Quit", button_color = "#808080", key = "-QUIT-")
     ]
 ]
 
@@ -77,7 +89,7 @@ parameter_list_column = [
 
 image_viewer_column = [
 
-    [sg.Image(key="-IMAGE-", filename=logo)]
+    [sg.Image(key="-IMAGE-", filename = logo)]
 
 ]
 
@@ -98,7 +110,7 @@ layout = [
 ]
 
 
-window = sg.Window("SimpleVNC Client", layout) #, icon='icon_square.ico')
+window = sg.Window("SimpleVNC Client", layout)
 
 # Run the Event Loop
 
@@ -113,11 +125,11 @@ while True:
     # Actual interface logic
 
     elif event == "-CONNECT-":
-        stop_client() # in case the server is restarted with the same button
+
+        stop_client() # in case the client is restarted with the same button
+
         try:
-            # si = subprocess.STARTUPINFO()
-            # si.dwFlags = subprocess.CREATE_NO_WINDOW
-            client = subprocess.Popen([sys.executable, 'clientwindow.py', addr, port, password])#, startupinfo=si)
+            client = subprocess.Popen([sys.executable, 'clientwindow.py', addr, port, password])
             status = Status.RUNNING
 
         except:
